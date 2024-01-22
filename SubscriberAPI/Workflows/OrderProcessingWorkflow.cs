f675bd2781bf40fcfee5e7b88fc8ca17e144d82a
Tomáš Vojtáš <tomas.vojtas@eset.com>
2024-01-22 16:03:37 +0100

    Added workflow statuses

--- SubscriberAPI/Workflows/OrderProcessingWorkflow.cs
+++ SubscriberAPI/Workflows/OrderProcessingWorkflow.cs
@@ -10,10 +10,12 @@ namespace SubscriberAPI.Workflows
         public override async Task<bool> RunAsync(WorkflowContext context, string input)
         {
             await context.CallActivityAsync<bool>(nameof(Activity1), input);
-            //Console.WriteLine($"After Activity1, waiting for Step1 '{context.InstanceId}'");
+            // Console.WriteLine($"After Activity1, waiting for Step1 '{context.InstanceId}'");
+            context.SetCustomStatus("After Activity1, Waiting for Event1");
             var event1 = await context.WaitForExternalEventAsync<Event1>(eventName: "event1");
             if (!event1.Success) return false;
             await context.CallActivityAsync<bool>(nameof(Activity2), input);
+            context.SetCustomStatus("After Activity2, Waiting for Event2");
             var event2 = await context.WaitForExternalEventAsync<Event1>(eventName: "event2");
             if (!event2.Success) return false;
             await context.CallActivityAsync<bool>(nameof(Activity3), input);
